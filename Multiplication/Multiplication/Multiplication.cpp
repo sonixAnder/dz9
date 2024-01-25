f2d17ff436fa8d5dbef8fbc5e0addaa72aa34339
sonixAnder <148888208+sonixAnder@users.noreply.github.com>
2024-01-25 16:51:53 +0300

    Add files via upload

---
+++ Multiplication/Multiplication/Multiplication.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() 
+{
+    setlocale(LC_ALL, "ru");
+    int num1;
+
+    cout << "Введите число, которое будем умножать: ";
+    cin >> num1;
+
+    cout << "Таблица умножения на " << num1 << ":" << endl;
+    for (int i = 1; i <= 10; i++) {
+        cout << num1 << " * " << i << " = " << num1 * i << endl;
+    }
+
+    return 0;
+}
